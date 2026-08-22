a047dd77c382d598c457e97db450c46e2a82252f
anas1213231 <2.07237839e+08+anas1213231@users.noreply.github.com>
2026-08-22 00:40:55 +0000

    Isolate host tests from app startup

--- HostTest/TiktigerHostTestAppDelegate.m
+++ HostTest/TiktigerHostTestAppDelegate.m
@@ -1,5 +1,4 @@
 #import "TiktigerHostTestAppDelegate.h"
-#import "TiktigerHostTestRunner.h"
 
 @implementation TiktigerHostTestAppDelegate
 
@@ -11,16 +10,6 @@
     controller.view.backgroundColor = UIColor.blackColor;
     self.window.rootViewController = controller;
     [self.window makeKeyAndVisible];
-
-    TiktigerHostTestRunner *runner = [[TiktigerHostTestRunner alloc] init];
-    NSError *lifecycleError = nil;
-    NSDictionary *lifecycle = [runner runLifecycleValidation:&lifecycleError];
-    NSError *compatibilityError = nil;
-    NSDictionary *compatibility = [runner runCompatibilityRecoveryValidation:&compatibilityError];
-    NSError *bindingError = nil;
-    BOOL binding = [runner validateBindingRoutesAndDiagnostics:&bindingError];
-    BOOL passed = [lifecycle[@"passed"] boolValue] && [compatibility[@"passed"] boolValue] && binding;
-    NSLog(@"TIKTIGER_HOST_TEST_RESULT passed=%@ lifecycle=%@ compatibility=%@ binding=%@ errors=%@/%@/%@", passed ? @"YES" : @"NO", lifecycle, compatibility, binding ? @"YES" : @"NO", lifecycleError.localizedDescription ?: @"", compatibilityError.localizedDescription ?: @"", bindingError.localizedDescription ?: @"");
     return YES;
 }
 
